7a968497e3df7c126d033eb6f41bc4d60adf6580
Beishui <32339094+Beishui@users.noreply.github.com>
2019-08-21 04:01:34 +0000

    Update HelloWorld.java

--- src/com/lyy/HelloWorld.java
+++ src/com/lyy/HelloWorld.java
@@ -2,7 +2,7 @@ package com.lyy;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-            System.out.println("Hello World! From LiYangyang develop2");
+            System.out.println("Hello World! From LiYangyang develop3");
 
 	}
 }
